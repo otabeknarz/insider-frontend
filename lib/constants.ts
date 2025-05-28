6561eb004319e97fa1ceecc6c33ce08d533b1aa7
Otabek Narz <otabeknarz@gmail.com>
2025-05-28 17:11:51 +0500

    BASE URL changed to api.ibratdebate.uz

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,7 @@
  */
 
 // API URLs
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://api.ibratdebate.uz";
 export const API_ENDPOINTS = {
   // Authentication
   AUTH: {
